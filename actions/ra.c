536e78c1eb53a4b7cd50df9659a6c6b554af2018
tblaase <tblaase@student.42heilbronn.de>
2021-08-26 17:53:37 +0200

    small changes again to test remote repo

--- actions/ra.c
+++ actions/ra.c
@@ -6,7 +6,7 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/19 17:44:01 by tblaase           #+#    #+#             */
-/*   Updated: 2021/08/19 17:56:13 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/08/26 17:53:07 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ void	ft_ra(t_stack **stack_a)
 {
 	t_stack	*temp;
 
-	temp = *stack_a->next;
+	// temp = *stack_a->next;
 
 	printf("%s", "\nThis is temp: ");
 	ft_display_list(temp);
 
-	*stack_a->next = NULL;
+	// *stack_a->next = NULL;
 	ft_lst_add_back(&temp, *stack_a);
 	*stack_a = temp;
 
